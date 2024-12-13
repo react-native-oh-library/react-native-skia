b07c30cc209b532631b0e0d44553e705fd1245ac
Mrliwang <Mrliwang@users.noreply.github.com>
2024-12-13 10:15:28 +0800

    release: @react-native-oh-tpl/react-native-skia@1.3.7-0.0.4 (#16)

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,11 +1,3 @@
-import "./skia/NativeSetup";
-export { JsiSkImage } from "./skia/web/JsiSkImage";
-export * from "./renderer";
-export * from "./renderer/Canvas";
-export * from "./renderer/Offscreen";
-export * from "./views";
-export * from "./skia";
-export * from "./external";
-export * from "./animation";
-export * from "./dom/types";
-export * from "./dom/nodes";
+import "@shopify/react-native-skia/src/skia/NativeSetup";
+export { JsiSkImage } from "@shopify/react-native-skia";
+export * from "@shopify/react-native-skia";
