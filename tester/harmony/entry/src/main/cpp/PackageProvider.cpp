ea9a861fc5f018bb81294e8315f588705bdbe3a0
Louis-C7 <chenlixi1@huawei.com>
2025-01-24 16:17:04 +0800

    chore: make the tester runnable

--- tester/harmony/entry/src/main/cpp/PackageProvider.cpp
+++ tester/harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,10 +1,14 @@
 #include "RNOH/PackageProvider.h"
 #include "ReanimatedPackage.h"
+#include "GestureHandlerPackage.h"
 #include "SkiaPackage.h"
 
 using namespace rnoh;
 
-std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(
-    Package::Context ctx) {
-  return {std::make_shared<SkiaPackage>(ctx), std::make_shared<ReanimatedPackage>(ctx),};
+std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
+    return {
+        std::make_shared<SkiaPackage>(ctx),
+        std::make_shared<GestureHandlerPackage>(ctx),
+        std::make_shared<ReanimatedPackage>(ctx),
+    };
 }
