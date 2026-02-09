eafb2e69780ad521ae6e5b26455384c23875931e
wupingyuan <wupingyuan2@h-partners.com>
2026-02-09 18:34:14 +0800

    perf:优化代码删除冗余代码
    Signed-off-by: wupingyuan <wupingyuan2@h-partners.com>

--- example/contexts.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
- * Use of this source code is governed by a MIT license that can be
- * found in the LICENSE file.
- */
-
-import React from 'react';
-
-export const AppParamsContext = React.createContext(undefined);
